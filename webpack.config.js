b57284f3d164a4a92115b921f49e0f7924a3c5bc
Harrison Bui <hsbui@hotmail.com>
2017-05-21 23:22:21 -0700

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./galactica.js",
+  output: {
+    filename: "./lib/bundle.js"
+  },
+  devtool: 'source-maps',
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+}
